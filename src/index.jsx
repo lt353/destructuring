3979ffd540af1b1241629367115d0debdb11ee20
Lindsay Trenton <lt353@hawaii.edu>
2025-04-14 12:32:19 -1000

    playing around with car data to practice destructuring

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./styles.css";
+import cars from "./practice.js";
+//import App from "./components/App";
+//import "./styles.css";
+
+const [honda, tesla] = cars;
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
+console.log(tesla);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />); 
+root.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+); 
